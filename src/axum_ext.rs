db70fa3d1f0378992b14913ac12b0ccd5d0e3801
rage <mathematicscpu@gmail.com>
2025-08-09 18:39:44 +0800

    add InvalidRequest

--- src/axum_ext.rs
+++ src/axum_ext.rs
@@ -9,8 +9,10 @@ impl IntoResponse for ServerError {
     fn into_response(self) -> Response {
         use axum::http::StatusCode;
         let internal_server_error = StatusCode::INTERNAL_SERVER_ERROR;
+        let bad_request = StatusCode::BAD_REQUEST;
         let res: Response = match self {
-            Self::HTTPParseError(o) => (StatusCode::BAD_REQUEST, o.to_string()).into_response(),
+            Self::InvalidRequest(o) => (bad_request, o.to_string()).into_response(),
+            Self::HTTPParseError(o) => (bad_request, o.to_string()).into_response(),
             #[cfg(feature = "sqlx_error")]
             Self::SQLXError(o) => {
                 (StatusCode::INTERNAL_SERVER_ERROR, o.to_string()).into_response()
--- src/lib.rs
+++ src/lib.rs
@@ -7,12 +7,15 @@ pub use thiserror::Error;
 
 #[derive(Error, Debug)]
 pub enum ServerError {
-    #[error("mutex lock error")]
-    MutexLockError,
+    #[error("Invalid Request: {0}")]
+    InvalidRequest(String),
 
     #[error("HTTP Parse: {0}")]
     HTTPParseError(#[from] http_parse::ParseError),
-    
+
+    #[error("mutex lock error")]
+    MutexLockError,
+
     #[cfg(feature = "sqlx_error")]
     #[error("SQLXError info: {0}")]
     SQLXError(#[from] sqlx::Error),
