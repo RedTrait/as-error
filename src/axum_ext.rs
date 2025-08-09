98ca9a66d592d73602f61010f622136df44b6112
rage <mathematicscpu@gmail.com>
2025-08-09 19:54:22 +0800

    add file error

--- src/axum_ext.rs
+++ src/axum_ext.rs
@@ -8,21 +8,36 @@ pub use crate::ServerError;
 impl IntoResponse for ServerError {
     fn into_response(self) -> Response {
         use axum::http::StatusCode;
-        let internal_server_error = StatusCode::INTERNAL_SERVER_ERROR;
-        let bad_request = StatusCode::BAD_REQUEST;
+
         let res: Response = match self {
             // TODO: https://developer.mozilla.org/en-US/docs/Web/HTTP/Reference/Status/412 Incomplete implementation
             // https://developer.mozilla.org/en-US/docs/Web/HTTP/Reference/Headers/If-Match
             // https://developer.mozilla.org/en-US/docs/Web/HTTP/Reference/Headers/If-Unmodified-Since
             // Consider adding header validation
             Self::PreconditionFailed(o) => (StatusCode::PRECONDITION_FAILED, o).into_response(),
-            Self::InvalidRequest(o) => (bad_request, o.to_string()).into_response(),
-            Self::HTTPParseError(o) => (bad_request, o.to_string()).into_response(),
+            Self::InvalidRequest(o) => (StatusCode::BAD_REQUEST, o.to_string()).into_response(),
+            Self::HTTPParseError(o) => (StatusCode::BAD_REQUEST, o.to_string()).into_response(),
             #[cfg(feature = "sqlx_error")]
             Self::SQLXError(o) => {
                 (StatusCode::INTERNAL_SERVER_ERROR, o.to_string()).into_response()
             }
-            Self::MutexLockError => internal_server_error.into_response(),
+            Self::MutexLockError => StatusCode::INTERNAL_SERVER_ERROR.into_response(),
+            #[cfg(feature = "file_error")]
+            Self::FileNotFound => (StatusCode::NOT_FOUND, "File not found").into_response(),
+            #[cfg(feature = "file_error")]
+            Self::FileAlreadyExists => {
+                (StatusCode::CONFLICT, "File already exists").into_response()
+            }
+            #[cfg(feature = "file_error")]
+            Self::FileFrozen => (StatusCode::FORBIDDEN, "File is frozen").into_response(),
+            #[cfg(feature = "file_error")]
+            Self::FileWriteFailed(o) => (StatusCode::INTERNAL_SERVER_ERROR, o).into_response(),
+            #[cfg(feature = "file_error")]
+            Self::FileReadFailed => {
+                (StatusCode::INTERNAL_SERVER_ERROR, "File read failed").into_response()
+            }
+            #[cfg(feature = "file_error")]
+            Self::FileDeleteFailed(o) => (StatusCode::INTERNAL_SERVER_ERROR, o).into_response(),
         };
         res
     }
--- src/lib.rs
+++ src/lib.rs
@@ -22,6 +22,30 @@ pub enum ServerError {
     #[cfg(feature = "sqlx_error")]
     #[error("SQLXError: {0}")]
     SQLXError(#[from] sqlx::Error),
+
+    #[cfg(feature = "file_error")]
+    #[error("FileNotFound")]
+    FileNotFound,
+
+    #[cfg(feature = "file_error")]
+    #[error("FileAlreadyExists")]
+    FileAlreadyExists,
+
+    #[cfg(feature = "file_error")]
+    #[error("FileFrozen")]
+    FileFrozen,
+
+    #[cfg(feature = "file_error")]
+    #[error("FileWriteFailed: {0}")]
+    FileWriteFailed(String),
+
+    #[cfg(feature = "file_error")]
+    #[error("FileReadFailed")]
+    FileReadFailed,
+
+    #[cfg(feature = "file_error")]
+    #[error("FileDeleteFailed: {0}")]
+    FileDeleteFailed(String),
 }
 
 pub type ResultExt<T> = Result<T, ServerError>;
