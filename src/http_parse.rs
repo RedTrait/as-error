7d08d5f51c559ac3a2715ccf908737d6b06b4be7
rage <mathematicscpu@gmail.com>
2025-08-09 15:28:31 +0800

    add http parse error

---
+++ src/http_parse.rs
@@ -0,0 +1,89 @@
+//! Currently using the definition in crate actix_web
+use std::{io, str::Utf8Error, string::FromUtf8Error};
+
+use derive_more::{Display, Error};
+use http::uri::InvalidUri;
+
+/// A set of errors that can occur during parsing HTTP streams.
+#[derive(Debug, Display, Error)]
+#[non_exhaustive]
+pub enum ParseError {
+    /// An invalid `Method`, such as `GE.T`.
+    #[display("invalid method specified")]
+    Method,
+
+    /// An invalid `Uri`, such as `exam ple.domain`.
+    #[display("URI error: {}", _0)]
+    Uri(InvalidUri),
+
+    /// An invalid `HttpVersion`, such as `HTP/1.1`
+    #[display("invalid HTTP version specified")]
+    Version,
+
+    /// An invalid `Header`.
+    #[display("invalid Header provided")]
+    Header,
+
+    /// A message head is too large to be reasonable.
+    #[display("message head is too large")]
+    TooLarge,
+
+    /// A message reached EOF, but is not complete.
+    #[display("message is incomplete")]
+    Incomplete,
+
+    /// An invalid `Status`, such as `1337 ELITE`.
+    #[display("invalid status provided")]
+    Status,
+
+    /// A timeout occurred waiting for an IO event.
+    #[allow(dead_code)]
+    #[display("timeout")]
+    Timeout,
+
+    /// An I/O error that occurred while trying to read or write to a network stream.
+    #[display("I/O error: {}", _0)]
+    Io(io::Error),
+
+    /// Parsing a field as string failed.
+    #[display("UTF-8 error: {}", _0)]
+    Utf8(Utf8Error),
+}
+
+impl From<io::Error> for ParseError {
+    fn from(err: io::Error) -> ParseError {
+        ParseError::Io(err)
+    }
+}
+
+impl From<InvalidUri> for ParseError {
+    fn from(err: InvalidUri) -> ParseError {
+        ParseError::Uri(err)
+    }
+}
+
+impl From<Utf8Error> for ParseError {
+    fn from(err: Utf8Error) -> ParseError {
+        ParseError::Utf8(err)
+    }
+}
+
+impl From<FromUtf8Error> for ParseError {
+    fn from(err: FromUtf8Error) -> ParseError {
+        ParseError::Utf8(err.utf8_error())
+    }
+}
+
+impl From<httparse::Error> for ParseError {
+    fn from(err: httparse::Error) -> ParseError {
+        match err {
+            httparse::Error::HeaderName
+            | httparse::Error::HeaderValue
+            | httparse::Error::NewLine
+            | httparse::Error::Token => ParseError::Header,
+            httparse::Error::Status => ParseError::Status,
+            httparse::Error::TooManyHeaders => ParseError::TooLarge,
+            httparse::Error::Version => ParseError::Version,
+        }
+    }
+}
