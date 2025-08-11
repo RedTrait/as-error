d346e6a448c130589bcbd8f6473298ee99d10179
rage <mathematicscpu@gmail.com>
2025-08-11 16:12:49 +0800

    fix: - InternalServiceError - StaticInternalServiceError add method on ServerError: - static_internal_service_error - internal_service_error

--- src/error/service.rs
+++ src/error/service.rs
@@ -2,6 +2,8 @@ use std::error::Error;
 
 use thiserror::Error;
 
+use crate::ServerError;
+
 #[derive(Error, Debug)]
 pub enum ServiceError {
     #[error("InvalidRequest: {0}")]
@@ -35,12 +37,61 @@ pub enum ServiceError {
     InternalServiceError(Box<dyn Error + Send + Sync>, String),
 
     #[error("StaticInternalServiceError: {0}")]
-    StaticInternalServiceError(Box<dyn Error + Send + Sync>, &'static str),
+    StaticInternalServiceError(Box<dyn Error + Send + Sync + 'static>, &'static str),
 }
 
 impl From<serde_json::Error> for ServiceError {
     fn from(_err: serde_json::Error) -> Self {
         // add error detail in log file, but not return detail to send req user
         ServiceError::StaticJsonError("Failed to parse JSON")
+    }
+}
+
+impl ServerError {
+    pub fn static_internal_service_error<E>(err: E, msg: &'static str) -> Self
+    where
+        E: Error + Send + Sync + 'static,
+    {
+        ServerError::ServiceError(ServiceError::StaticInternalServiceError(Box::new(err), msg))
+    }
+
+    pub fn internal_service_error<E, S>(err: E, msg: S) -> Self
+    where
+        E: Error + Send + Sync + 'static,
+        S: Into<String>,
+    {
+        ServerError::ServiceError(ServiceError::InternalServiceError(
+            Box::new(err),
+            msg.into(),
+        ))
+    }
+}
+
+#[cfg(test)]
+pub mod tests {
+    use super::*;
+
+    #[derive(thiserror::Error, Debug)]
+    pub enum TestThisError {
+        #[error("could not generate image")]
+        TestError,
+    }
+
+    #[derive(derive_more::Display, derive_more::Error, Debug)]
+    pub enum TestderiveMoreError {
+        #[display("TestderiveMoreError")]
+        TestError,
+    }
+
+    #[test]
+    fn internal_service_error() {
+        ServerError::internal_service_error(TestThisError::TestError, "H".to_owned());
+        ServerError::internal_service_error(TestderiveMoreError::TestError, "H".to_owned());
+    }
+
+    #[test]
+    fn static_internal_service_error() {
+        ServerError::static_internal_service_error(TestThisError::TestError, "H");
+        ServerError::static_internal_service_error(TestderiveMoreError::TestError, "H");
     }
 }
