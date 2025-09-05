cedc72aea4ef83355fad93e133889e3aaef721bb
rage <mathematicscpu@gmail.com>
2025-09-05 15:32:46 +0800

    add serde_json_error example

--- examples/service_error.rs
+++ examples/service_error.rs
@@ -1,10 +1,27 @@
 use as_error::*;
+use serde::{Deserialize, Serialize};
 use thiserror::Error;
 
 #[derive(Error, Debug)]
 pub enum SignUpError {
     #[error("normal avatar not create")]
     NormalAvatarNotCreate,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Point {
+    x: i32,
+    y: i32,
+}
+
+fn serde_json_error() -> ResultExt<Point> {
+    let point = Point { x: 1, y: 2 };
+    let mut serialized =
+        serde_json::to_string(&point).map_err(|e| Into::<ServiceError>::into(e))?;
+    serialized.push_str("}"); // make it invalid JSON
+    let deserialized: Point =
+        serde_json::from_str(&serialized).map_err(|e| Into::<ServiceError>::into(e))?;
+    Ok(deserialized)
 }
 
 fn main() {
@@ -15,4 +32,6 @@ fn main() {
     let info = String::from("This is a string error");
     let error = precondition_failed!(e, info);
     println!("Error: {}", error);
+    let error = serde_json_error();
+    println!("Error: {:?}", error);
 }
